5d803bb4a1a2d73535a6a335d579020bad89bf00
Hrant Nurijanyan <hrant.nurijanyan@gmail.com>
2022-11-11 01:23:53 +0400

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+// Project generated my cmaker
+int main()
+{
+    std::cout << "Hello OpenGL" << std::endl;
+    return 0;
+}
